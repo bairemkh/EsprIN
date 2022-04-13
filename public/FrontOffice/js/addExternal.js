968b3bde2047499bd267e2fae2eb7f79930dcb01
Bairem <bairemkh@gmail.com>
2022-04-13 01:28:51 +0100

    Add announce completed

--- public/FrontOffice/js/addExternal.js
+++ public/FrontOffice/js/addExternal.js
@@ -6,26 +6,22 @@ const externIdF = document.getElementById('comIdF');
 const submitExtern = document.getElementById('submitExtern');
 
 function verifyExtern(){
-    console.log("inside verify extern")
     let externName=externNameF.value;
     let externEmail=externEmailF.value;
     let externLocation=externLocationF.value;
     let externPassword=externPasswordF.value;
     let externId=externIdF.value;
-    const pattern=/^[\w!#$%&'*+/=?`{|}~^-]+(?:\.[\w!#$%&'*+/=?`{|}~^-]+)*@(?:[a-zA-Z0-9-]+\.)+[a-zA-Z]{2,6}$/;
-    if(externName==="") {
-        setErrorFor(externNameF, "Name cannot be null");
-        console.log("extern name")
-    }
+    if(externName==="")
+        setErrorFor(externNameF,"Name cannot be null");
     if(externId==="")
         setErrorFor(externIdF,"ID cannot be null");
-    if((externEmail==="")||(!pattern.test(externEmail)))
-        setErrorFor(externEmailF,"Check your email please");
+    if(externEmail==="")
+        setErrorFor(externEmailF,"Email cannot be null");
     if(externLocation==="")
         setErrorFor(externLocationF,"Location cannot be null");
     if(externPassword=== "")
         setErrorFor(externPasswordF,"Password cannot be null");
-    if((externName!=="")&&(externEmail!=="")&&(externLocation!=="")&&(externPassword!=="")&&(externId!=="")&&(externId!=="")&&(pattern.test(externEmail))){
+    if((externName!=="")&&(externEmail!=="")&&(externLocation!=="")&&(externPassword!=="")&&(externId!=="")&&(externId!=="")){
         removeErrorFor(externNameF);
         removeErrorFor(externEmailF);
         removeErrorFor(externLocationF);
@@ -38,7 +34,6 @@ function verifyExtern(){
 submitExtern.addEventListener("click",()=>{
     console.log("Entered event")
     verifyExtern();
-    console.log("exited event")
 })
 function setErrorFor(errorInput, msg) {
     let formControl = errorInput.parentElement;
